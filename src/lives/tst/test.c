bc34ed9658309438862ae87bad4a21a57cd48fba
maherme <maherme.dev@gmail.com>
2026-03-31 18:08:25 +0200

    Add lives (#41)

---
+++ src/lives/tst/test.c
@@ -0,0 +1,30 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2026 Manuel Hernández Méndez
+ *
+ * Authors:
+ *   Manuel Hernández Méndez <maherme.dev@gmail.com>
+ */
+
+#include "testLives.h"
+#include <cmocka.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+
+int
+main(void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test(testLivesCreate),
+        cmocka_unit_test(testLivesRemoveLastLive),
+        cmocka_unit_test(testLivesRemovePenultimateLive),
+        cmocka_unit_test(testLivesRemoveMaxLivesPrinted),
+        cmocka_unit_test(testLivesAdd),
+        cmocka_unit_test(testLivesAddMaxLivesPrinted),
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
